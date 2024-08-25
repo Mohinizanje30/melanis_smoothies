50a544eb8824340b62b2602eebd9611f18ad10a9
Mohini Zanje <73573828+Mohinizanje30@users.noreply.github.com>
2024-08-25 23:18:35 +0530

    Update streamlit_app.py
    typing error in connection string

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.write(
     """Choose the fruit you want to add in your custom smoothie!
     """
 )
-cnx=st.connection("Snowflake")
+cnx=st.connection("snowflake")
 session=cnx.session()
 name_on_order=st.text_input("Name on Smoothie")
 st.write("The name on smoothie will be", name_on_order)
